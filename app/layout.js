20a32df62ce057300350cc8bbf6f196a177d14b1
romvillalba <rominavillalbap5@gmail.com>
2023-12-08 20:58:13 -0300

    branch prueba

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Jdeco',
+  title: 'JCdeco',
   description: 'Velas',
 }
 
--- components/footer.jsx
+++ components/footer.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-center space-x-4 mt-4 mb-4">
-      <Link href="/" className="flex"><InstagramIcon />@Jcdeco</Link>
+      <Link href="/" className="flex"><InstagramIcon />@jcdeco.rox</Link>
       <Link href="/" className="flex"><PhoneIcon />+54911xxxxxxxxx</Link>
     </div>
   );
