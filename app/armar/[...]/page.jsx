265e2c2e8f3cd04500a958fc532eca584ce3a7a4
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-12-09 17:32:53 -0300

    imagenes por props

--- app/armar/[...]/page.jsx
+++ app/armar/[...]/page.jsx
@@ -1,12 +1,26 @@
 import EmblaCarousel from "@/components/ui/embla-carousel"
 
 const { CandleForm } = require("../components/candleForm")
-
+const images = [
+  {
+    src: "/img/candle_2.webp",
+    alt: "Vela 1",
+  },
+  {
+    src: "/img/candle_3.webp",
+    alt: "Vela 2",
+  },
+  // ...
+  {
+    src: "/img/candle_4.webp",
+    alt: "Vela n",
+  },
+];
 
 const page = () => {
   return (
     <div className="flex flex-col lg:flex-row gap-20 justify-center items-center py-12">
-      <EmblaCarousel/>
+      <EmblaCarousel slides={images}/>
       <CandleForm/>
     </div>
   )
