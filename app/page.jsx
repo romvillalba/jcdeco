90da91ba56ef7298ae92a10b26c1dbaa73bc510c
Reynoso <reynoso.geronimo@gmail.com>
2023-12-15 11:21:11 -0300

    preparando para db

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,96 @@
 import CandleCard from "@/components/candleCard";
 
-export default function Home() {
+async function getData() {
+  /*const res = await fetch("https://api.example.com/...");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();*/
+  const frascos = [
+    {
+      id: 1,
+      name: "Vidrio cilíndrico",
+      description:
+        "Vidrio cilíndrico de 250 ml. Ideal para velas de té, velas de jarra o velas votivas.",
+      destacado: true,
+      images: [
+        {src:"/img/candle_2.webp",alt:"vela"},
+        {src:"/img/candle_3.webp",alt:"vela"},
+        {src:"/img/candle_4.webp",alt:"vela"},
+        ],
+    },
+    {
+      id: 2,
+      name: "Vidrio redondo",
+      description:
+        "Vidrio redondo de 300 ml. Ideal para velas de pilar o velas de tarro.",
+      destacado: true,
+      images: [
+        {src:"/img/candle_2.webp",alt:"vela"},
+        {src:"/img/candle_3.webp",alt:"vela"},
+        {src:"/img/candle_4.webp",alt:"vela"},
+        ],
+    },
+    {
+      id: 3,
+      name: "Vidrio hexagonal",
+      description:
+        "Vidrio hexagonal de 200 ml. Ideal para velas de té, velas de jarra o velas votivas.",
+      destacado: true,
+      images: [
+        {src:"/img/candle_2.webp",alt:"vela"},
+        {src:"/img/candle_3.webp",alt:"vela"},
+        {src:"/img/candle_4.webp",alt:"vela"},
+        ],
+    },
+    {
+      id: 4,
+      name: "Vidrio de boca ancha",
+      description:
+        "Vidrio de boca ancha de 500 ml. Ideal para velas de pilar o velas de tarro.",
+      destacado: true,
+      images: [
+        {src:"/img/candle_2.webp",alt:"vela"},
+        {src:"/img/candle_3.webp",alt:"vela"},
+        {src:"/img/candle_4.webp",alt:"vela"},
+        ],
+    },
+    {
+      id: 5,
+      name: "Vidrio con base de corcho",
+      description:
+        "Vidrio con base de corcho de 100 ml. Ideal para velas de té, velas de jarra o velas votivas.",
+      destacado: true,
+      images: [
+        {src:"/img/candle_2.webp",alt:"vela"},
+        {src:"/img/candle_3.webp",alt:"vela"},
+        {src:"/img/candle_4.webp",alt:"vela"},
+        ],
+    },
+    {
+      id: 6,
+      name: "Vidrio con base de madera",
+      description:
+        "Vidrio con base de madera de 250 ml. Ideal para velas de pilar o velas de tarro.",
+      destacado: true,
+      images: [
+        {src:"/img/candle_2.webp",alt:"vela"},
+        {src:"/img/candle_3.webp",alt:"vela"},
+        {src:"/img/candle_4.webp",alt:"vela"},
+        ],
+    },
+  ];
+
+  return frascos;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <main className="flex flex-wrap justify-center items-center px-12 py-24 gap-6 md:gap-14 min-h-[600px]">
-      <CandleCard id={2} />
-      <CandleCard id={3} />
-      <CandleCard id={4} />
-      <CandleCard id={5} />
+      {data.map(frasco => (
+        <CandleCard id={frasco.id} name={frasco.name}description={frasco.description}images={frasco.images} key={frasco.id} />
+      ))}
     </main>
   );
 }
--- components/candleCard.jsx
+++ components/candleCard.jsx
@@ -14,30 +14,17 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Carousel from "./ui/carousel";
 
-const CandleCard = ({ id }) => {
-  const images = [
-    {
-      src: "/img/candle_2.webp",
-      alt: "Vela 1",
-    },
-    {
-      src: "/img/candle_3.webp",
-      alt: "Vela 2",
-    },
-    // ...
-    {
-      src: "/img/candle_4.webp",
-      alt: "Vela n",
-    },
-  ];
+const CandleCard = ({ id,name,description,images }) => {
+  
+
 
   return (
-    <Card className="text-center px-0 space-x-0 border-orange-500 " >
+    <Card className="text-center px-0 space-x-0 border-orange-500 w-[260px]" >
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle className={"text-sm"}>{name}</CardTitle>
+        <CardDescription className={"whitespace-pre-line text-xs h-12"}>{description}</CardDescription>
       </CardHeader>
-      <CardContent className="relative h-[280px] w-[260px]">
+      <CardContent className="relative ">
         <Carousel images={images} />
       </CardContent>
       <CardFooter>
@@ -46,16 +33,7 @@ const CandleCard = ({ id }) => {
         </Button>
       </CardFooter>
     </Card>
-    // <div className="flex-col text-center items-center justify-center rounded-lg p-2 border-solid border-2 border-orange-500 ">
-    //   <div className="relative h-[280px] w-[260px]">
-    //     <Carousel images={images} />
-    //   </div>
-
-    //   <h2>Texto de la Vela</h2>
-    //   <Button asChild size="lg">
-    //     <Link href={`/armar/envase=${id}`}>Personalizar</Link>
-    //   </Button>
-    // </div>
+    
   );
 };
 
